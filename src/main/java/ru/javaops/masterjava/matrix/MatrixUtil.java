02af9d64e5c6a2c25abc27364a4189fff0fb8a3b
niochronos <niochronos@gmail.com>
2017-10-23 20:56:42 +0300

    - Refactored class MatrixUtil.java  - removed class MultiplyMatrixTask  - refactored class MultiplyResult

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -11,27 +11,63 @@ import java.util.concurrent.*;
  */
 public class MatrixUtil {
 
-    // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
-        final CompletionService<List<MultiplyResult>> completionService = new ExecutorCompletionService<List<MultiplyResult>>(executor);
+
+        class MultiplyResult {
+            final int colNumber;
+            final int[] elements;
+
+            public MultiplyResult(int colNumber, int[] elements) {
+                this.colNumber = colNumber;
+                this.elements = elements;
+            }
+        }
+
+        final CompletionService<MultiplyResult> completionService = new ExecutorCompletionService<MultiplyResult>(executor);
 
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        final List<Future<List<MultiplyResult>>> futures = new ArrayList<>(matrixSize);
+        final List<Future<MultiplyResult>> futures = new ArrayList<>(matrixSize);
 
-        for (int col = 0; col < matrixSize; col++) {
-            futures.add(completionService.submit(new MultiplyMatrixTask(matrixA, matrixB, col, matrixSize)));
+        for (int j = 0; j < matrixSize; j++) {
+            final int col = j;
+            futures.add(completionService.submit(new Callable<MultiplyResult>() {
+                @Override
+                public MultiplyResult call() throws Exception {
+                    int elements[] = new int[matrixSize];
+
+                    int thatColumn[] = new int[matrixSize];
+
+                    for (int k = 0; k < matrixSize; k++) {
+                        thatColumn[k] = matrixB[k][col];
+                    }
+
+                    for (int i = 0; i < matrixSize; i++) {
+                        int thisRow[] = matrixA[i];
+                        int sum = 0;
+                        for (int k = 0; k < matrixSize; k++) {
+                            sum += thisRow[k] * thatColumn[k];
+                        }
+                        elements[i] = sum;
+                    }
+
+                    return new MultiplyResult(col, elements);
+                }
+            }));
         }
 
         while (!futures.isEmpty()) {
-            Future<List<MultiplyResult>> listFuture = completionService.poll();
-            if (listFuture != null) {
-                List<MultiplyResult> multiplyResultList = listFuture.get();
-                futures.remove(listFuture);
+            Future<MultiplyResult> future = completionService.poll();
+            if (future != null) {
+                MultiplyResult multiplyResult = future.get();
+                futures.remove(future);
 
-                for (MultiplyResult multiplyResult : multiplyResultList) {
-                    matrixC[multiplyResult.row][multiplyResult.col] = multiplyResult.sum;
+                int col = multiplyResult.colNumber;
+                int length = multiplyResult.elements.length;
+                int elements[] = multiplyResult.elements;
+                for (int i = 0; i < length; i++) {
+                    matrixC[i][col] = elements[i];
                 }
             }
         }
@@ -39,7 +75,6 @@ public class MatrixUtil {
         return matrixC;
     }
 
-    // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
@@ -86,53 +121,5 @@ public class MatrixUtil {
             }
         }
         return true;
-    }
-
-    private static class MultiplyResult {
-        final int row;
-        final int col;
-        final int sum;
-
-        public MultiplyResult(int row, int col, int sum) {
-            this.row = row;
-            this.col = col;
-            this.sum = sum;
-        }
-    }
-
-    private static class MultiplyMatrixTask implements Callable<List<MultiplyResult>> {
-        final int[][] matrixA;
-        final int[][] matrixB;
-        final int col;
-        final int matrixSize;
-
-        public MultiplyMatrixTask(int[][] matrixA, int[][] matrixB, int col, int matrixSize) {
-            this.matrixA = matrixA;
-            this.matrixB = matrixB;
-            this.col = col;
-            this.matrixSize = matrixSize;
-        }
-
-        @Override
-        public List<MultiplyResult> call() throws Exception {
-            final List<MultiplyResult> list = new ArrayList<>(matrixSize);
-
-            int thatColumn[] = new int[matrixSize];
-
-            for (int k = 0; k < matrixSize; k++) {
-                thatColumn[k] = matrixB[k][col];
-            }
-
-            for (int i = 0; i < matrixSize; i++) {
-                int thisRow[] = matrixA[i];
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += thisRow[k] * thatColumn[k];
-                }
-                list.add(new MultiplyResult(i, col, sum));
-            }
-
-            return list;
-        }
     }
 }
