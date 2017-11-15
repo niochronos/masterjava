6a30db6a078a9484e8032ab529e095278edebfde
niochronos <niochronos@gmail.com>
2017-11-15 19:52:40 +0200

    Dona HW04

--- persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
@@ -2,6 +2,7 @@ package ru.javaops.masterjava.persist.dao;
 
 import com.bertoncelj.jdbi.entitymapper.EntityMapperFactory;
 import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.customizers.BatchChunkSize;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import ru.javaops.masterjava.persist.model.User;
 
@@ -29,6 +30,11 @@ public abstract class UserDao implements AbstractDao {
 
     @SqlQuery("SELECT * FROM users ORDER BY full_name, email LIMIT :it")
     public abstract List<User> getWithLimit(@Bind int limit);
+
+    @SqlBatch("INSERT INTO users (full_name, email, flag) VALUES (:fullName, :email, CAST(:flag AS user_flag)) " +
+            "ON CONFLICT (email) DO NOTHING ")
+    @BatchChunkSize(20)
+    public abstract void insertBatch(@BindBean List<User> users);
 
     //   http://stackoverflow.com/questions/13223820/postgresql-delete-all-content
     @SqlUpdate("TRUNCATE users")
--- web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UploadServlet.java
@@ -1,6 +1,9 @@
 package ru.javaops.masterjava.upload;
 
+import org.skife.jdbi.v2.tweak.ConnectionFactory;
 import org.thymeleaf.context.WebContext;
+import ru.javaops.masterjava.persist.DBIProvider;
+import ru.javaops.masterjava.persist.dao.UserDao;
 import ru.javaops.masterjava.persist.model.User;
 
 import javax.servlet.ServletException;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.DriverManager;
 import java.util.List;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
@@ -19,6 +23,10 @@ import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 @WebServlet(urlPatterns = "/", loadOnStartup = 1)
 @MultipartConfig
 public class UploadServlet extends HttpServlet {
+
+    static {
+        initDBI();
+    }
 
     private final UserProcessor userProcessor = new UserProcessor();
 
@@ -40,12 +48,33 @@ public class UploadServlet extends HttpServlet {
             }
             try (InputStream is = filePart.getInputStream()) {
                 List<User> users = userProcessor.process(is);
-                webContext.setVariable("users", users);
+
+                UserDao dao = DBIProvider.getDao(UserDao.class);
+
+                dao.insertBatch(users);
+
+//                webContext.setVariable("users", users);
+                webContext.setVariable("users", dao.getWithLimit(20));
                 engine.process("result", webContext, resp.getWriter());
             }
         } catch (Exception e) {
             webContext.setVariable("exception", e);
             engine.process("exception", webContext, resp.getWriter());
         }
+    }
+
+    private static void initDBI() {
+        initDBI("jdbc:postgresql://localhost:5432/masterjava", "user", "password");
+    }
+
+    private static void initDBI(String dbUrl, String dbUser, String dbPassword) {
+        DBIProvider.init(() -> {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("PostgreSQL driver not found", e);
+            }
+            return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        });
     }
 }
