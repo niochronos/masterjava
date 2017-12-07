b0bdcde86585a51876b5998977605efb9ce42288
niochronos <niochronos@gmail.com>
2017-12-07 18:45:36 +0200

    Patch 7_1_HW6_MailSender

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
@@ -3,6 +3,7 @@ package ru.javaops.masterjava.service.mail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * gkislin
@@ -12,6 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Addressee {
-    private String email;
+    private @NonNull String email;
     private String name;
+
+    public Addressee(String email) {
+        this(email, null);
+    }
 }
